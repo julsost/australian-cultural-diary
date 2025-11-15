77c23d7eacff6f4b2d635dcbd247cbaa2c40cd42
Juliet Sostena <julietsostena@Juliets-MacBook-Pro-10.local>
2025-11-15 21:37:29 +1000

    Fix Vite base path for GitHub Pages

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/australian-cultural-diary/', // 👈 VERY IMPORTANT
+  base: '/australian-cultural-diary-juliet-sostena/', // 👈 VERY IMPORTANT
 })
